e1515810c5f737debae181a6b11eef6d47e2aa9f
RKMyst <141899255+RKMyst@users.noreply.github.com>
2024-02-19 19:37:59 -0500

    Delete ex1_test.py

--- ex1_test.py
+++
@@ -1,7 +0,0 @@
-import write_your_name as name_point
-
-def test_hi_my_name_is():
-    # Check if the length of the name is greater than 1 (not an empty string)
-    assert len(name_point.hi_my_name_is()) > 1
-
-
